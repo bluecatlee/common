4b04c26e2d7c37e40f3430c9c187ecfb341f36c5
BluecatLee <jiaobujun@163.com>
2019-06-20 11:20:31 +0800

    del

--- src/main/java/com/github/bluecatlee/common/lock/LockedObject.java
+++
@@ -1,9 +0,0 @@
-package com.github.bluecatlee.common.lock;
-
-import java.lang.annotation.*;
-
-@Target(ElementType.PARAMETER)
-@Retention(RetentionPolicy.RUNTIME)
-@Documented
-public @interface LockedObject {
-}
